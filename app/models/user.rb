f318de39912a7e6e03cf11d22d04003d5b2c94ba
Keisha S. Heeralal <keishaheeralal@gmail.com>
2013-10-06 20:16:56 -0400

    Add and test validation for last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :statuses
 
   validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def full_name
     first_name + " " + last_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   test "a user should enter a first name" do
     user = User.new
     assert !user.save
     assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
   end
 end
